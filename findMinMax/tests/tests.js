38874704f1825300fc7f868ca41de5507d895e49
celestine <okorocelestine@gmail.com>
2017-01-07 08:16:20 +0100

    Change the test framework from mocha to jasmine for findMinMax test script

--- findMinMax/tests/tests.js
+++ findMinMax/tests/tests.js
@@ -1,23 +1,22 @@
-(function() {
+(function(){
   'use strict';
-  var chai = require('chai'),
-      expect = chai.expect,
-      myAttempt = require('../app/library.js');
+    var chai = require('chai'),
+      myfindMinMaxAttempt = require('../app/library.js');
 
   describe('Min-Max Numbers in a List: ', function () {
 
     describe('Return the min and max number in the list in a new list follows `[min, max]`', function () {
 
       it('should return [1,4] for [1, 2, 3 , 4]', function () {
-        expect(myAttempt.findMinMax([1, 2, 3, 4])).to.equal([1, 4]);
+        expect([1,4]).toEqual(myfindMinMaxAttempt.findMinMax([1, 2, 3, 4]));
       });
 
       it('should return [4, 6] for [6, 4]', function () {
-        expect(myAttempt.findMinMax([6, 4])).to.equal([4, 6]);
+        expect(myfindMinMaxAttempt.findMinMax([6, 4])).toEqual([4, 6]);
       });
 
       it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
-        expect(myAttempt.findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).to.equal([2, 78]);
+        expect(myfindMinMaxAttempt.findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2,78]);
       });
 
     });
@@ -25,41 +24,41 @@
     describe('Return the number in the list in a new list follows `[min]` when the number is the min and max number in that list', function () {
 
       it('should return [4] for [4, 4, 4, 4]', function () {
-        expect(myAttempt.findMinMax([4, 4, 4, 4])).to.equal([4]);
+        expect(myfindMinMaxAttempt.findMinMax([4, 4, 4, 4])).toEqual([4]);
       });
       it('should return [2,2.000] for [2,2.000]', function () {
-        expect(myAttempt.findMinMax([2,2.000] )).to.equal([2] );
+        expect(myfindMinMaxAttempt.findMinMax([2,2.000] )).toEqual([2] );
       });
 
     });
 
     describe('Return an error messsage for argument  that are not array', function () {
 
-      it('should return "expecting an array of numbers" for [a, b, c]', function () {
-        expect(myAttempt.findMinMax(['a', 'b', 'c'])).to.equal("expecting an array of numbers");
+      it('should return "expecting an array of  numbers only " for [a, b, c]', function () {
+        expect(myfindMinMaxAttempt.findMinMax(['a', 'b', 'c'])).toEqual("expecting an array of numbers only");
       });
-      it('should return "expecting an array of numbers" for [4, 6, c]', function () {
-        expect(myAttempt.findMinMax([4, 6, 'c'])).to.equal("expecting an array of numbers");
+      it('should return "expecting an array of numbers only" for [4, 6, c]', function () {
+        expect(myfindMinMaxAttempt.findMinMax([4, 6, 'c'])).toEqual("expecting an array of numbers only");
       });
 
     });
 
     describe('Return the min and max number in the list with negative numbers in a new list follows `[min, max]`', function () {
       it('should return [-4,-1] for [-4, -3, -2, -1]', function () {
-        expect(myAttempt.findMinMax([-4, -3, -2, -1])).to.equal([-4,-1]);
+        expect(myfindMinMaxAttempt.findMinMax([-4, -3, -2, -1])).toEqual([-4,-1]);
       });
       it('should return [-4,0] for [-4, -3, -2, -1,0]', function () {
-        expect(myAttempt.findMinMax([-4, -3, -2, -1,0])).to.equal([-4,0]);
+        expect(myfindMinMaxAttempt.findMinMax([-4, -3, -2, -1,0])).toEqual([-4,0]);
       });
 
     });
 
     describe('Return the min and max number in the list with decimal numbers in a new list follows `[min, max]`', function () {
-      it('should return [4.0,4.001] for [4.0, 4.0000013,4.0001]', function () {
-        expect(myAttempt.findMinMax([4.0, 4.0000013,4.0001])).to.equal([4.0,4.001] );
+      it('should return [4.0,4.001] for [4.0000011, 4.0000013,4.0001]', function () {
+        expect(myfindMinMaxAttempt.findMinMax([4.0000011, 4.0000013,4.0001])).toEqual([4.0000011,4.0001] );
       });
       it('should return [2,2.001] for [2,2.001,2.00001]', function () {
-        expect(myAttempt.findMinMax([2,2.001,2.00001])).to.equal([2,2.001] );
+        expect(myfindMinMaxAttempt.findMinMax([2,2.001,2.00001])).toEqual([2,2.001] );
       });
 
     });
