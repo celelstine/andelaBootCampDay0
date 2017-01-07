7b170041c968dc557a110ea974ae97e570bde13c
celestine <okorocelestine@gmail.com>
2017-01-07 05:49:58 +0100

    Add library.js for the findMinMax labtest

---
+++ findMinMax/app/library.js
@@ -0,0 +1,41 @@
+/**
+ * A function to return the min and max numeber(s) in a array
+ * @Author okorocelstine@gmail.com (Okoro Celestine)
+ */
+'use strict'
+
+var submit = {}
+submit.findMinMax = function(num) {
+
+	var min = 0,
+	    max =0,
+	    cur,
+	    minMax=[];
+	if ( typeof num[0] !== 'number') {
+	  return "expecting an array of numbers only";
+	} else {
+	  min = max = num[0];
+	  	for(var i=1; i<num.length;i++) {
+	  		cur = num[i];
+	  		if (typeof cur !== 'number') {
+	  			return "expecting an array of numbers only";
+	  		} else if (cur <min) {
+	  			min = cur;
+	  		} else if (cur >max) {
+	  			max=cur;
+	  		}
+	  		
+	  	}
+
+	  	if (min !== max){
+	  		minMax.push(min);
+	  		minMax.push(max);
+	  	} else {
+	  		minMax.push(min);
+	  	}
+    return minMax;
+	}
+}
+
+
+module.exports = submit;
